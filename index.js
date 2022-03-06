dc90f2bad71b8c6c2a14ad70283660bfb2220145
akashbind12 <97519781+akashbind12@users.noreply.github.com>
2022-03-06 18:01:20 +0530

    intial comment

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+const app = express();
+
+
+app.get("/", function (req, res){
+    res.send("hello");
+});
+
+app.get("/books", function (req, res){
+    res.send([{"id":1,"book_name":"Harri","book_author":"Shandie"},
+    {"id":2,"book_name":"Rivkah","book_author":"Crissie"},
+    {"id":3,"book_name":"Gifford","book_author":"Shaw"},
+    {"id":4,"book_name":"Chariot","book_author":"Nathanael"}]);
+});
+
+app.listen(6002, () => {
+console.log("listening at port 6002")
+});
